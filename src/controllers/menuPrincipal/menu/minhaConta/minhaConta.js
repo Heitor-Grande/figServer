73fe36bdd5e02a219f3c2fff3885244dd0e6cc93
Heitor-Grande <heitorogrande2001@gmail.com>
2024-09-13 14:32:13 -0300

    atualização de conta e avatar

--- src/controllers/menuPrincipal/menu/minhaConta/minhaConta.js
+++ src/controllers/menuPrincipal/menu/minhaConta/minhaConta.js
@@ -1,6 +1,7 @@
 const controllerMinhaConta = require("express").Router()
 const dbconnection = require("../../../../database/dbconnection")
 const { verificaJWTLogin } = require("../../../../functions/JWTS")
+//carrega informações da conta
 controllerMinhaConta.get("/carregar/usuario/:idUsuario", verificaJWTLogin, async function (req, res) {
     try {
         const idUsuario = req.params.idUsuario
@@ -26,6 +27,25 @@ controllerMinhaConta.get("/carregar/usuario/:idUsuario", verificaJWTLogin, async
         })
     }
 })
-
-
+//criar rota put de informações da conta
+controllerMinhaConta.put("/atualizar/minha/conta/:idUsuario", verificaJWTLogin, async function (req, res) {
+    try {
+        const inputsConta = req.body.inputsConta
+        const SqlUpdateConta = `UPDATE public.usuario
+        SET
+        nome = '${inputsConta.nome}',
+        email= '${inputsConta.email}',
+        avatar = '${inputsConta.avatar}'
+        WHERE id_usuario = ${req.params.idUsuario}
+        `
+        await dbconnection.query(SqlUpdateConta)
+        return res.status(200).send({
+            message: "Informações atualizadas com sucesso."
+        })
+    } catch (error) {
+        return res.status(500).send({
+            message: "Erro ao atualizar conta: " + error.message || error
+        })
+    }
+})
 module.exports = controllerMinhaConta

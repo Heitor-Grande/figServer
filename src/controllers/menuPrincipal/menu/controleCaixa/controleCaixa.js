de971063ca7fecf180341b165b6724e3163b57ac
Heitor-Grande <heitorogrande2001@gmail.com>
2024-09-14 18:04:48 -0300

    crud movimentação finalizado

--- src/controllers/menuPrincipal/menu/controleCaixa/controleCaixa.js
+++ src/controllers/menuPrincipal/menu/controleCaixa/controleCaixa.js
@@ -39,6 +39,7 @@ controllerControleCaixa.get("/carregar/movimentos/caixa/:id_usuario", verificaJW
         FROM public.movimentos
         WHERE
         id_usuario = ${req.params.id_usuario}
+        order by id_movimento DESC
         `
         const movimentos = (await dbconnection.query(SqlSelectMovimento)).rows
         return res.status(200).send({
@@ -48,6 +49,59 @@ controllerControleCaixa.get("/carregar/movimentos/caixa/:id_usuario", verificaJW
     } catch (error) {
         return res.status(500).send({
             message: "Erro ao carregar movimentos: " + error.message || error
+        })
+    }
+})
+//carrega informações de um determinado movimento
+controllerControleCaixa.get("/carregar/detalhes/movimento/:id_usuario/:id_movimento", verificaJWTLogin, async function (req, res) {
+    try {
+        const { id_usuario, id_movimento } = req.params
+        const SqlSelectMovimento = `
+        SELECT 
+        titulo, 
+        id_movimento as id,
+        valor,
+        datamovimento,
+        tipo,
+        id_usuario
+        FROM public.movimentos
+        WHERE
+        id_usuario = ${id_usuario} AND
+        id_movimento = ${id_movimento}
+        `
+        const movimento = (await dbconnection.query(SqlSelectMovimento)).rows[0]
+        return res.status(200).send({
+            movimento: movimento
+        })
+    } catch (error) {
+        return res.status(500).send({
+            message: "Erro ao carregar: " + error.message || error
+        })
+    }
+})
+//atualiza movimento
+controllerControleCaixa.put("/atualizar/movimento/:id_usuario/:id_movimento", verificaJWTLogin, async function (req, res) {
+    try {
+        const inputsMovimento = req.body.inputsMovimento
+        const id_usuario = req.body.id_usuario
+        const id_movimento = req.params.id_movimento
+        const SqlUpdateMovimento = `
+        UPDATE public.movimentos
+        SET
+        titulo = '${inputsMovimento.titulo}',
+        tipo = '${inputsMovimento.tipo}',
+        valor = ${inputsMovimento.valor.replaceAll(".", "").replace(",", ".")}, 
+        datamovimento = '${inputsMovimento.data}'
+        WHERE
+        id_usuario = ${id_usuario} AND id_movimento = ${id_movimento}
+        `
+        await dbconnection.query(SqlUpdateMovimento)
+        return res.status(200).send({
+            message: "Movimento atualizado"
+        })
+    } catch (error) {
+        return res.status(500).send({
+            message: "Erro ao atualizar: " + error.message || error
         })
     }
 })
